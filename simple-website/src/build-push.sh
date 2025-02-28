4470ad9a7ebddd9e9ca2cd64eddec298619f49dd
sprevacomm <sean.patnode@intellibridge.us>
2025-02-27 21:33:36 -1000

    Add simple website to test build, push and deploy

---
+++ simple-website/src/build-push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set variables
+VERSION="1.0.0"
+IMAGE_NAME="devopsone808/simple-website:$VERSION"
+
+# Build the Docker image
+echo "Building Docker image: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+# Push to Docker Hub
+echo "Pushing to Docker Hub..."
+docker push $IMAGE_NAME
+
+echo "Complete! Image pushed to $IMAGE_NAME"
